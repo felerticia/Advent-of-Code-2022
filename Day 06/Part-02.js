6ecd2dd3b220ae918a8e7b948c7903457cf76215
felerticia <felerticia@gmail.com>
2022-12-06 09:08:18 +0100

    Add files via upload

--- Day 06/Part-02.js
+++ Day 06/Part-02.js
@@ -1,12 +1,12 @@
 const fs = require ('fs')
 
-const BUFFER_SIZE = 14
+const CHUNK_SIZE = 14
 
 const findMarker = str => {
     for (let i = 0; i < str.length; i++) {
-        const sub = str.substring(i,i+BUFFER_SIZE)
-        if ([...new Set(sub)].length === BUFFER_SIZE)
-            return i+BUFFER_SIZE
+        const sub = str.substring(i,i+CHUNK_SIZE)
+        if ([...new Set(sub)].length === CHUNK_SIZE)
+            return i+CHUNK_SIZE
     }
 }
 
